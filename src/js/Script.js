c8c5e4bc2f1bc3975200ab0579ef7bdf24a73e47
Carlos8743 <lourenco7898.g6@gmail.com>
2023-12-05 17:06:36 -0300

    Atualização de estilos e Design responsivo

--- src/js/Script.js
+++ src/js/Script.js
@@ -5,44 +5,50 @@ const Save = document.querySelector('.Page_save');
 const buttonSend = document.querySelector('.Page_send');
 const buttonSave = document.querySelector('.Page_save');
 const Pokemon = document.querySelector('.Pokemon');
+const Fail = document.querySelector('.Erro');
 /* Events clicks and butttom*/
-form.addEventListener('submit', (e)=> 
-
-e.preventDefault());
+form.addEventListener('submit', (e)=> e.preventDefault());
 // buttonSave.addEventListener('click', Save_point);
 buttonSend.addEventListener('click', Set_atribute);
 
 async function Get_API_Pokemon(name){
-    name =  name.toLowerCase();
-    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
-    .then(data => data.json())
-    .catch(err =>{
-        console.log(typeof err, err);
-    });
-    return pokemon
+    if(name.length > 4 && name != '1000'){
+        Fail.innerText = 'O Numero tem que ser entre 1 e 1000';
+        return null
+    }
+    if(name){
+        name =  name.toLowerCase();
+        const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then(data => data.json())
+        .catch(err =>{
+            console.log(typeof err, err);
+        });
+        return pokemon
+    }else{
+        return null
+    }
 }
 async function Set_atribute(){
     const json = await Get_API_Pokemon(Input.value);
     if(!json){
-        console.log('A função nao tem valor')
+        Fail.innerText += "\n Pokemon não encontrado";
+        Fail.style.display = 'flex';
+        Pokemon.innerHTML = '';
+        Input.value = '';
+        Input.focus();
     }else{
+        Fail.innerText = "";
+        Fail.style.display = 'none';
         Pokemon.innerHTML = `            
             <li class="Pokemon_img"><img src="${json.sprites.other[`official-artwork`].front_default}" alt="Imagem Do Pokemon"></li>
             <li class="Pokemon_name">Nome: ${json.name}</li>
-            <li class="Pokemon_Type">Tipo: ${json.types[0].type.name}</li>
-            <li class="Pokemon_Atk"> Primeiro Ataque: ${json.abilities[0].ability.name}</li>
-            <li class="Pokemon_Special">Especial: ${json}</li>
+            <li class="Pokemon_Type">Tipo: ${json.types[0].type.name} e ${json.types[1]?.type.name ?? "Não possui segundo ataque"}</li>
+            <li class="Pokemon_Atk">Ataque: ${json.abilities[0].ability.name} e ${json.abilities[1]?.ability.name ?? "Não possui segunda abilidade"}</li>
         `;
-        if(json.abilities[1].ability.name){
-            Pokemon.innerHTML += `
-            <li class="Pokemon_Atk2">Sengundo Ataque: ${json.abilities[1].ability.name}</li>
-            `;
-        }
     }
-    console.log(json);
-}
-
+    }
 function Save_point(point){
     let Data = [];
     data.push({name:point});
 }
+
